fb8d64b1a53aee2424d3d07a272af206070b7998
Wiktor Gola <wiktor.gola.95@wp.pl>
2022-12-08 19:05:21 +0100

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const grid = document.querySelector(".gallery");
+
+galleryItems.forEach((image) => {
+  const newDiv = document.createElement("div");
+  const newLink = document.createElement("a");
+  const newItem = document.createElement("img");
+
+  newDiv.classList.add("gallery__item");
+  grid.append(newDiv);
+  newLink.classList.add("gallery__link");
+  newDiv.append(newLink);
+  newItem.classList.add("gallery__image");
+  newItem.setAttribute("src", `${image.preview}`);
+  newItem.setAttribute("alt", `${image.description}`);
+  newItem.setAttribute("data-source", `${image.original}`);
+  newLink.append(newItem);
+});
+
+grid.addEventListener("click", (event) => {
+  if (event.target.nodeName === "IMG") {
+    const instance = basicLightbox.create(`
+      <img src="${event.target.dataset.source}" width="800" height="600">
+      `);
+    instance.show();
+
+    document.addEventListener("keydown", (event) => {
+      if (event.key === "Escape") {
+        instance.close();
+      };
+    });
+  };
+});
