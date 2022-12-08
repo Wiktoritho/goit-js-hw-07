0f70f6e581e2e2e407f294e02ddfa3b407164040
Wiktor Gola <wiktor.gola.95@wp.pl>
2022-12-08 19:05:24 +0100

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,16 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const grid = document.querySelector(".gallery");
+
+galleryItems.forEach((img) => {
+    grid.insertAdjacentHTML("beforeend", `<a class="gallery__item" href="${img.original}">
+  <img class="gallery__image" src="${img.preview}" alt="${img.description}" />
+</a>`)
+});
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
